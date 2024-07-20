f380625c0e11746a710e8526def0933efbdf2378
arshkhatwani <arsh.khatwani@gmail.com>
2024-07-20 13:13:23 +0530

    add ans btn

--- src/components/AnsBtn.tsx
+++ src/components/AnsBtn.tsx
@@ -4,7 +4,7 @@ function AnsBtn({ label }: { label: "Correct" | "Incorrect" }) {
     return (
         <div>
             <button
-                className={`text-5xl text-center text-white bg-${bgColor}-500 px-5 py-3 rounded-lg font-semibold hover:bg-${bgColor}-700`}>
+                className={`w-36 md:w-60 text-2xl md:text-4xl flex justify-center items-center text-white bg-${bgColor}-500 px-5 py-3 rounded-lg font-semibold hover:bg-${bgColor}-700`}>
                 {label}
             </button>
         </div>
