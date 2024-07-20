0ee02f4cadf18d2c64982abbc9a7caa4f68e4c69
arshkhatwani <arsh.khatwani@gmail.com>
2024-07-20 13:22:24 +0530

    update layout

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,26 @@
-import './App.css'
+import "./App.css";
+import AnsBtn from "./components/AnsBtn";
+import QuestionNum from "./components/QuestionNum";
+import StatusBtn from "./components/StatusBtn";
 
 function App() {
+    return (
+        <div className="flex flex-col justify-between min-h-screen py-5">
+            <div className="pt-20">
+                <QuestionNum />
 
-  return (
-    <h1>hello world</h1>
-  )
+                <div className="mt-40 flex justify-evenly">
+                    <AnsBtn label="Correct" />
+                    <AnsBtn label="Incorrect" />
+                </div>
+            </div>
+
+            <div className="flex justify-evenly gap-20">
+                <StatusBtn label="Reset" />
+                <StatusBtn label="Complete" />
+            </div>
+        </div>
+    );
 }
 
-export default App
+export default App;
