628eeafd2de3e6ef94d72bcd7b70c42b2691f55f
arshkhatwani <arsh.khatwani@gmail.com>
2024-07-20 13:15:17 +0530

    add component for question number

---
+++ src/components/QuestionNum.tsx
@@ -0,0 +1,9 @@
+export default function QuestionNum() {
+    return (
+        <div>
+            <h1 className="text-center text-5xl md:text-6xl font-bold text-white">
+                Question #1
+            </h1>
+        </div>
+    );
+}
